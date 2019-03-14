2b96a8b9f03746c83b826d446c762caac854cc3e
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-14 21:35:55 +0100

    Updates

---
+++ precache-manifest.59c2ee438a1ac6999cda15cf34969a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  },
+  {
+    "revision": "5f508ab3f3cd5133f5ce",
+    "url": "/weatherapp/static/js/main.f3e586bb.chunk.js"
+  },
+  {
+    "revision": "d7c4e2fa51b196da5eb2",
+    "url": "/weatherapp/static/js/2.76d7b653.chunk.js"
+  },
+  {
+    "revision": "5f508ab3f3cd5133f5ce",
+    "url": "/weatherapp/static/css/main.cf1fa452.chunk.css"
+  },
+  {
+    "revision": "d7c4e2fa51b196da5eb2",
+    "url": "/weatherapp/static/css/2.d2405d9d.chunk.css"
+  },
+  {
+    "revision": "5230570bc12f9d04ade5d39cba6073f7",
+    "url": "/weatherapp/index.html"
+  }
+];
