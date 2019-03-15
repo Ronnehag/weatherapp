8f10aadffd9d454388bf07ff3af1f146f58dbbec
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-15 09:36:52 +0100

    fixed styling on loadingbar

--- src/components/Layout/LoadingBar.js
+++ src/components/Layout/LoadingBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoadingBar() {
     return (
         <div className="row">
-            <div className="col s7 offset-s5">
+            <div className="col s12 l8 offset-l2">
                 <div className="progress">
                     <div className="indeterminate"></div>
                 </div>
