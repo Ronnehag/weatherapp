baf03e38d74e1c9f2208364eaefd577ca7121c8f
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-03-15 09:32:35 +0100

    switched to hashrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import "materialize-css";
 import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 
 const routing = (
-    <Router>
+    <HashRouter>
         <div>
-            <Route exact path="/" component={App} />
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
         </div>
-    </Router>
+    </HashRouter>
 )
 
 ReactDOM.render(routing, document.getElementById('root'))
